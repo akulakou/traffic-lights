23b14c3893d78eaef6c5747b0782f328a5a56603
anton <anton@MacBook-Air-anton.local>
2023-01-02 22:24:12 +0300

    Initial commit

---
+++ apps/green-wave-chain/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'green-wave-chain',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/green-wave-chain',
+};
